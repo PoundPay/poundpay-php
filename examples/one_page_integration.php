9e3d458b5ff08fc20d8fc45bff9ff8dc8ddf7d7f
Matin Tamizi <mtamizi@gmail.com>
2011-01-23 23:47:01 -0800

    super easy one page integration of iframe

---
+++ examples/one_page_integration.php
@@ -0,0 +1,36 @@
+<?php
+  require "poundpay.php";
+
+  $account_sid = 'DVxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+  $auth_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+  $api_uri = 'http://api-sandbox.poundpay.com';
+  $www_uri = 'http://www-sandbox.poundpay.com';
+
+  $client = new PoundPayAPIClient($account_sid, $auth_token, $api_uri);
+  $data = array(
+    "amount" => 20000, // in USD cents
+    "payer_fee_amount" => 0,
+    "recipient_fee_amount" => 500,
+    "recipient_email_address" => "david@example.com",
+    "description" => "Beats by Dr. Dre (White)",
+  );
+
+  $response = $client->request("/payment_requests/", "POST", $data);
+  $payment_request = $response->response_json;
+?>
+
+<html>
+  <title>Simple Marketplace</title>
+  <script src="<?php echo $www_uri; ?>/js/pmp/pound_pmp.js"></script>
+  <body>
+    <h1>Simple Marketplace</h1>
+    <h2><?php echo $payment_request->description; ?></h2>
+    <div id="pound-root"></div>
+    <script type="text/javascript">
+      PoundPayment.init({
+        payment_request_sid: "<?php echo $payment_request->sid; ?>",
+        server: "<?php echo $www_uri; ?>"
+      })
+    </script>
+  </body>
+</html>
