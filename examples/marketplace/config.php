3aeb32d0ccd8ce3a397242eb0baf8c483897b1a6
Matin Tamizi <mtamizi@gmail.com>
2011-01-24 21:25:26 -0800

    typo

--- examples/marketplace/config.php
+++ examples/marketplace/config.php
@@ -4,7 +4,7 @@ $CONFIG = array(
     'api_uri' => 'https://api-sandbox.poundpay.com',
     'www_uri' => 'https://www-sandbox.poundpay.com',
     'version' => 'silver',
-    'sid' => 'DVxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    'sid' => 'DVxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     'auth_token' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
   ),
 );
