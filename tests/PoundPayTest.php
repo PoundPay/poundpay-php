2be575e28c0e229f3a41673fb485ed65dc50ca89
Russell Davis <russell.davis@gmail.com>
2011-03-20 16:50:50 -0700

    Unit test stub

---
+++ tests/PoundPayTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once dirname(__FILE__).'/../poundpay.php';
+require_once 'PHPUnit/Framework.php';
+
+class HipChatPHPTest extends PHPUnit_Framework_TestCase {
+
+  public function testLoad() {
+    echo 'hi';
+  }
+
+
+}
