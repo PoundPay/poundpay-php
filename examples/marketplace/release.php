ffc1f22aaf126b134b3f18932e94fb7137d21efa
Russell Davis <russell.davis@gmail.com>
2011-03-20 17:50:48 -0700

    update for refactored client library

--- examples/marketplace/release.php
+++ examples/marketplace/release.php
@@ -1,21 +1,21 @@
 <?php
-/*
-GET /release.php?payment_sid=<PYxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx>
-Releases the escrowed money to the recipient
-*/
 
 require 'config.php';
-require 'poundpay.php';
+require dirname(__FILE__).'/../../poundpay.php';
 
-$poundpay_client = new PoundPayAPIClient($CONFIG['poundpay']['sid'],
-                                         $CONFIG['poundpay']['auth_token'],
-                                         $CONFIG['poundpay']['api_uri'],
-                                         $CONFIG['poundpay']['version']);
-$payment_sid = $_GET['payment_sid'];
+PoundPay\configure(
+    $CONFIG['poundpay']['sid'],
+    $CONFIG['poundpay']['auth_token'],
+    $CONFIG['poundpay']['api_uri'],
+    $CONFIG['poundpay']['version']
+);
 
 // Release payment
-$response = $poundpay_client->request("/payments/{$payment_sid}", 'PUT', array('status' => 'released'));
-$payment = $response->response_json;
+$payment = new PoundPay\Payment(array(
+    'sid' => $_GET['payment_sid'],
+    'status' => 'released'
+));
+$payment->save();
 ?>
 
 <html>
@@ -26,6 +26,6 @@ $payment = $response->response_json;
     <h1>Release Payment</h1>
     <h2>$<?= $payment->amount / 100 ?> was released for <?= $payment->description ?></h2>
     <h2>PoundPay Response for Payment</h2>
-    <pre><?= $response->response_text ?></pre>
+    <pre><?= PoundPay\getLastResponse()->body ?></pre>
   </body>
 </html>
