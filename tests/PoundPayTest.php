fb657175cb9bcb660da3694f3cf4416e27353e6a
Russell Davis <russell.davis@gmail.com>
2011-03-26 20:31:17 -0700

    unit tests: add save() tests, refactor

--- tests/PoundPayTest.php
+++ tests/PoundPayTest.php
@@ -5,7 +5,7 @@ require_once 'PHPUnit/Autoload.php';
 
 class PoundPayTest extends PHPUnit_Framework_TestCase {
 
-    public function get_production_config() {
+    protected function getConfig() {
         return array(
             'developer_sid' => 'DV0383d447360511e0bbac00264a09ff3c',
             'auth_token' => 'c31155b9f944d7aed204bdb2a253fef13b4fdcc6ae15402004',
@@ -13,37 +13,40 @@ class PoundPayTest extends PHPUnit_Framework_TestCase {
             'api_version' => 'silver'
         );
     }
+}
 
-    public function get_sandbox_config() {
-        return array(
-            'developer_sid' => 'DV0383d447360511e0bbac00264a09ff3c',
-            'auth_token' => 'c31155b9f944d7aed204bdb2a253fef13b4fdcc6ae15402004',
-            'api_url' => 'https://api-sandbox.zzpoundpay.com',
-            'api_version' => 'gold'
-        );
-    }
+class ResourceTest extends PoundPayTest {
 
+    protected $config;
+    protected $client;
+    
     public function resourceProvider() {
         return array(array('developers', 'PoundPay\\Developer'),
                      array('payments', 'PoundPay\\Payment'));
     }
 
-    /** @dataProvider resourceProvider */
-    public function testFind($resource, $class) {
-        $config = $this->get_production_config();
-        $client = $this->getMockBuilder('PoundPay\APIClient')
+    protected function setUp() {
+        $this->config = $this->getConfig();
+        $this->client = $this->getMockBuilder('PoundPay\APIClient')
                        ->disableOriginalConstructor()
                        ->getMock();
-        PoundPay\Resource::setClient($client);
+        PoundPay\Resource::setClient($this->client);
+    }
 
+    protected function makeApiResponse($data, $status_code = 200) {
+        return new PoundPay\APIResponse($this->config['api_url'], json_encode($data), $status_code);
+    }
+
+    /** @dataProvider resourceProvider */
+    public function testFind($resource, $class) {
         $findSid = 'foo';
         $findAttrs = array('foo' => 'bar');
-        $clientRetVal = new PoundPay\APIResponse($config['api_url'], json_encode($findAttrs), 200);
+        $apiResponse = $this->makeApiResponse($findAttrs);
 
-        $client->expects($this->once())
-               ->method('get')
-               ->with($this->equalTo("$resource/$findSid"))
-               ->will($this->returnValue($clientRetVal));
+        $this->client->expects($this->once())
+                     ->method('get')
+                     ->with($this->equalTo("$resource/$findSid"))
+                     ->will($this->returnValue($apiResponse));
 
         $foundObj = $class::find($findSid);
         $createdObj = new $class($findAttrs);
@@ -52,27 +55,55 @@ class PoundPayTest extends PHPUnit_Framework_TestCase {
 
     /** @dataProvider resourceProvider */
     public function testAll($resource, $class) {
-        $config = $this->get_production_config();
-        $client = $this->getMockBuilder('PoundPay\APIClient')
-                       ->disableOriginalConstructor()
-                       ->getMock();
-        PoundPay\Resource::setClient($client);
-
         $allAttrs = array(array('foo' => 'bar'), array('foo2' => 'bar2'));
-        $jsonResponse = json_encode(array($resource => $allAttrs));
-        $clientRetVal = new PoundPay\APIResponse($config['api_url'], $jsonResponse, 200);
+        $apiResponse = $this->makeApiResponse(array($resource => $allAttrs));
 
-        $client->expects($this->once())
-               ->method('get')
-               ->with($this->equalTo($resource))
-               ->will($this->returnValue($clientRetVal));
+        $this->client->expects($this->once())
+                     ->method('get')
+                     ->with($this->equalTo($resource))
+                     ->will($this->returnValue($apiResponse));
 
         $all = $class::all();
-        $refAll = array();
+        $expectedAll = array();
         foreach ($allAttrs as $attrs) {
-            $refAll[] = new $class($attrs);
+            $expectedAll[] = new $class($attrs);
         }
-        $this->assertEquals($refAll, $all);
+        $this->assertEquals($expectedAll, $all);
+    }
+
+    /** @dataProvider resourceProvider */
+    public function testSaveUpdate($resource, $class) {
+        $sid = 'DVxxx';
+        $initVars = array('sid' => $sid, 'k1' => 'v1', 'k2' => 'v2');
+        $responseVars = array('k3' => 'v3', 'k4' => 'v4');
+        $resourceObj = new $class($initVars);
+        $apiResponse = $this->makeApiResponse($responseVars);
+
+        $this->client->expects($this->once())
+                     ->method('put')
+                     ->with($this->equalTo("$resource/$sid"), $this->equalTo($initVars))
+                     ->will($this->returnValue($apiResponse));
+
+        $resourceObj->save();
+        $expectedObj = new $class($initVars + $responseVars);
+        $this->assertEquals($resourceObj, $expectedObj);
+    }
+
+    /** @dataProvider resourceProvider */
+    public function testSaveCreate($resource, $class) {
+        $initVars = array('k1' => 'v1', 'k2' => 'v2');
+        $responseVars = array('k3' => 'v3', 'k4' => 'v4');
+        $resourceObj = new $class($initVars);
+        $apiResponse = $this->makeApiResponse($responseVars);
+
+        $this->client->expects($this->once())
+                     ->method('post')
+                     ->with($this->equalTo($resource), $this->equalTo($initVars))
+                     ->will($this->returnValue($apiResponse));
+
+        $resourceObj->save();
+        $expectedObj = new $class($initVars + $responseVars);
+        $this->assertEquals($resourceObj, $expectedObj);
     }
 
 }
